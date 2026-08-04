5cb576ef69181ea0690eb1e4a84550d9f2c7e0da
Trung Nguyen <dtrung2702@gmail.com>
2026-08-05 02:33:34 +0700

    Update VehicleRepository.ts

--- src/repositories/VehicleRepository.ts
+++ src/repositories/VehicleRepository.ts
@@ -1,8 +1,38 @@
-import { JsonFileRepository } from './JsonFileRepository';
-import { Vehicle } from '../domain/Vehicle';
+import { JsonFileRepository } from "./JsonFileRepository";
+import {
+    Vehicle,
+    VehicleType
+} from "../domain/Vehicle";
 
-export class VehicleRepository extends JsonFileRepository<Vehicle & { id: string }> {
-  constructor() {
-    super('src/data/vehicles.json');
-  }
+export class VehicleRepository extends JsonFileRepository<Vehicle> {
+
+    constructor() {
+        super("vehicles.json");
+    }
+
+    public async findAvailableByType(branchId: string, type: VehicleType): Promise<Vehicle[]> {
+        const vehicles = await this.findAll();
+
+        return vehicles.filter(vehicle =>
+            vehicle.branchId === branchId &&
+            vehicle.type === type &&
+            vehicle.isAvailable()
+        );
+    }
+
+    public async findByBranch(branchId: string): Promise<Vehicle[]> {
+        const vehicles = await this.findAll();
+
+        return vehicles.filter(vehicle =>
+            vehicle.branchId === branchId
+        );
+    }
+
+    public async findAvailable(): Promise<Vehicle[]> {
+        const vehicles = await this.findAll();
+
+        return vehicles.filter(vehicle =>
+            vehicle.isAvailable()
+        );
+    }
 }
